1063f624a1e1bab3f4c0b5f80713dd6574f18480
Ivan Kristianto <ivan.g.kristianto@hotmail.com>
2019-12-22 15:44:17 +0700

    update integration

--- Ui-Kit/AdditionalClasses/UIControlSubscription+UIControlPublisher.swift
+++ Ui-Kit/AdditionalClasses/UIControlSubscription+UIControlPublisher.swift
@@ -47,3 +47,13 @@ public struct UIControlPublisher<Control: UIControl>: Publisher {
         subscriber.receive(subscription: subscription)
     }
 }
+
+// MARK: Extension
+/// Extending the `UIControl` types to be able to produce a `UIControl.Event` publisher.
+public protocol CombineCompatible { }
+extension UIControl: CombineCompatible { }
+public extension CombineCompatible where Self: UIControl {
+    func publisher(for events: UIControl.Event) -> UIControlPublisher<UIControl> {
+        return UIControlPublisher(control: self, events: events)
+    }
+}
